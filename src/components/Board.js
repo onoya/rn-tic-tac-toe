16aef43ae13478d4149560b87fcc1a1e60af8e7e
Naoya Ono <ono.naoyaa@gmail.com>
2019-06-08 16:37:21 +0800

    Upgrade to Expo SDK 33

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { GameConsumer } from '../GameContext';
+import GameContext from '../GameContext';
 import Cell from './Cell';
 
-const Board = () => (
-  <GameConsumer>
-    {({ board, handleCellPress }) => (
-      <View style={styles.board}>
-        {board.map((cell, i) => (
-          <Cell key={i} cell={cell} onPress={handleCellPress(i)} />
-        ))}
-      </View>
-    )}
-  </GameConsumer>
-);
+const Board = () => {
+  const { board, handleCellPress } = useContext(GameContext);
+
+  return (
+    <View style={styles.board}>
+      {board.map((cell, i) => (
+        <Cell key={i} cell={cell} onPress={handleCellPress(i)} />
+      ))}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   board: {
